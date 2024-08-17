f818a7504b4e24db632a0aa5e18b96545d9a4da9
Muhammad Shoaib Mushtaq <shoaibfattani2010@gmail.com>
2024-08-17 20:48:39 +0500

    added sweet alert popup on successful operations

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,3 +1,60 @@
+<<<<<<< HEAD
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import MUITable from "../components/MUITable";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
+
+const Home = () => {
+  const [usersData, setUsersData] = useState([]);
+  //   console.log(usersData);
+
+  const navigate = useNavigate();
+
+  const fetchUsersData = () => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((response) => setUsersData(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchUsersData();
+  }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:3000/users/${id}`)
+      .then(() => {
+        Swal.fire({
+          title: 'Success!',
+          text: 'User Deleted Successfully ',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        })
+        fetchUsersData();
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <>
+      <h1 style={{ display: "inline-block" }}>Users Data</h1>
+      <Button
+        variant="contained"
+        sx={{ float: "right", marginTop: "25px" }}
+        onClick={() => navigate("/create")}
+      >
+        Create New User
+      </Button>
+      <MUITable data={usersData} onDelete={handleDelete} />
+    </>
+  );
+};
+
+export default Home;
+=======
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -47,3 +104,4 @@ const Home = () => {
 };
 
 export default Home;
+>>>>>>> 48feb52e1b2647dbdd59120054de747e02436133
--- src/screens/UpdateUser.jsx
+++ src/screens/UpdateUser.jsx
@@ -1,3 +1,101 @@
+<<<<<<< HEAD
+import { useParams, useNavigate } from "react-router-dom";
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Swal from 'sweetalert2'
+
+const UpdateUser = () => {
+  const { id } = useParams();
+  console.log(id);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/users/${id}`)
+      .then((res) => setUpdatedUser(res.data))
+      .catch((err) => console.log(err));
+  },[]);
+
+  const [updatedUser, setUpdatedUser] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.put(`http://localhost:3000/users/${id}`, updatedUser)
+    .then(()=> {
+      Swal.fire({
+        title: 'Success!',
+        text: 'User Updated Successfully ',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      })
+      navigate("/");
+    })
+    .catch((err) => console.log(err));
+  };
+
+  return (
+    <Paper
+      elevation={22}
+      sx={{ width: "50%", margin: "30px auto", padding: "20px" }}
+    >
+      <Box>
+        <Typography sx={{ mb: "20px", textAlign: "center" }}>
+          Update Existing User
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            type="text"
+            fullWidth
+            value={updatedUser.name}
+            required
+            sx={{ mb: "20px" }}
+            onChange={(e) =>
+              setUpdatedUser({ ...updatedUser, name: e.target.value })
+            }
+          />
+          <TextField
+            type="text"
+            fullWidth
+            value={updatedUser.username}
+            required
+            sx={{ mb: "20px" }}
+            onChange={(e) =>
+              setUpdatedUser({ ...updatedUser, username: e.target.value })
+            }
+          />
+          <TextField
+            type="email"
+            fullWidth
+            value={updatedUser.email}
+            required
+            sx={{ mb: "20px" }}
+            onChange={(e) =>
+              setUpdatedUser({ ...updatedUser, email: e.target.value })
+            }
+          />
+          <TextField
+            type="text"
+            fullWidth
+            value={updatedUser.phone}
+            required
+            sx={{ mb: "20px" }}
+            onChange={(e) =>
+              setUpdatedUser({ ...updatedUser, phone: e.target.value })
+            }
+          />
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Paper>
+  );
+};
+
+export default UpdateUser;
+=======
 import { useParams, useNavigate } from "react-router-dom";
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import axios from "axios";
@@ -88,3 +186,4 @@ const UpdateUser = () => {
 };
 
 export default UpdateUser;
+>>>>>>> 48feb52e1b2647dbdd59120054de747e02436133
